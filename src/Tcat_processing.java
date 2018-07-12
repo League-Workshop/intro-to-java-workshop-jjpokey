8825a9c720d180f91e546c6cb0cab87053e30350
League <league@WTS8.attlocal.net>
2018-07-12 12:01:57 -0700

    Day4

---
+++ src/Tcat_processing.java
@@ -0,0 +1,32 @@
+/*PImage catPic;
+int a = 232;
+int b = 150;
+int acceleration = 5;
+void setup() {
+  size(620, 413);
+  catPic = loadImage("cat.jpg");
+  catPic.resize(620,413);
+  background(catPic);
+}
+  
+
+
+ void draw() {
+noStroke();
+ellipse(a,b,70,40);
+fill(017,238,000);
+ellipse(a+150,b,70,40);
+fill(017,238,000);
+if(a < 0) {
+  background(catPic);
+  a = 232;
+  b = 150;
+  acceleration = 5;
+}
+
+ }
+  
+void keyPressed() {
+  a-=2*acceleration;
+  b-=2*acceleration;
+}*/
--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,38 +16,44 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot mini = new Robot();
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		mini.setAngle(0);
+		mini.microMove(10);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		mini.setAngle(180);
+		mini.microMove(10);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+		mini.setAngle(270);
+		mini.microMove(10);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		mini.setAngle(90);
+		mini.microMove(10);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = mini.getX();
+		int robotYLocation = mini.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println(robotXLocation);
+		System.out.println(robotYLocation);
 		// 7. If robot is at same location as the little girl
-		//      --make a pop-up tell the robot where to go next
-		
+		if(robotXLocation == 720 && robotYLocation == 430) {
+JOptionPane.showMessageDialog(null, "Search near the skull...");
+		}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
 		
